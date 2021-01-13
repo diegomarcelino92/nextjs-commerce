d995a6fdfa8a498ba04ca1d6ae334a8f69408850
Diego Marcelino <diego@smartbr.com>
2021-01-13 12:58:47 -0300

    Configuração styled, material, criação layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,22 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { AppProps } from 'next/app';
+
+import { ThemeProvider } from '@material-ui/core';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+
+import theme from '../src/configuration/theme';
+
+import Layout from '../src/components/Layout';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <StyledThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </StyledThemeProvider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp;
+export default App;
